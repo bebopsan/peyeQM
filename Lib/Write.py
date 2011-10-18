a86f9232efff5853d1f5272bcf311ab259fada07
Santiago <santiag77e@gmail.com>
2011-10-18 14:54:17 -0500

    * Modified for use with 1D probles using Gmsh format. * Functionality added for the creation of the .geo file for 1D mesh * Comments and structure for better visualization.

--- Lib/Write.py
+++ Lib/Write.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python
 ## module Write
 '''
-    WriteVTK(points,triangles,gradient,Output).
+    WriteVTK(Nodes,Elems,gradient,Output).
    f
-    points:
+    Nodes:
                 Array of coordinates for each node.
-    triangles:
+    Elems:
                 Array of conectivities between nodes. For this case
                 sets of three nodes.
     gradient:
@@ -21,56 +21,134 @@ from numscan import numscan
 import numpy as np
 from numpy import linspace as lin
 from numpy import hstack
-def WriteVTK(Output,points,triangles,grad=0):
+def WriteVTK(Output,Nodes,Elems,grad=0):
 
-    nEle=str(np.shape(triangles)[0])
-    nPoi=str(np.shape(points)[0])
-    sizeEle=str(np.size(triangles))
+    nEle=str(np.shape(Elems)[0])
+    nPoi=str(np.shape(Nodes)[0])
+    sizeEle=str(np.size(Elems))
     f = open(Output, 'w')
     f.write('# vtk DataFile Version 3.1 \n')
     f.write('Results\n')
     f.write('ASCII\n')
     f.write('DATASET POLYDATA\n')
     
-    f.write(('POINTS '+nPoi+' DOUBLE\n'))
-    np.savetxt(f,points,fmt='%6.6f')
+    f.write(('Nodes '+nPoi+' DOUBLE\n'))
+    np.savetxt(f,Nodes,fmt='%6.6f')
     f.write(('TRIANGLE_STRIPS '+nEle+' '+sizeEle+' \n'))
 
-    np.savetxt(f,triangles,fmt='%d')
+    np.savetxt(f,Elems,fmt='%d')
     if grad!=0:
         f.write(('POINT_DATA '+nPoi+' \n'))
         f.write('VECTORS flechas double\n')
         np.savetxt(f,gradient,fmt='%+f %+f %d')
-def WriteMSH(Output,points,triangles):
+def WriteMSH(Output,Nodes,Elems,physicalEnt='line'):
+    """
+        This funtion intends to create a file of gmsh format containing
+        the characteristics of a given mesh and its inherent object.
 
-    nEle=str(np.shape(triangles)[0])
-    nPoi=str(np.shape(points)[0])
-    sizeEle=str(np.size(triangles))
+        For now this program accepts nodes and elements from  a 1D problem
+        and returns the files
+
+        Parameters:
+        -----------
+                    Output      Name given for the output file
+                    Nodes       Nodes
+                    Elems   Elements
+                    Physical entities   lines (for now)
+        Returns:
+        ----------
+                       Output.msh           Nodes and elements in gmsh format 
+                       Output.geo           Physical entities contained
+    """
+
+    nEle=str(np.shape(Elems)[0])
+    sizeEle=str(np.size(Elems))
+    nNodes=Nodes.shape[0]
+    # Creates the output file for the mesh
     f = open(Output, 'w')
+    # Creates the output file for the geometry by stripping the original name
+    # and adding the .geo format
+    g = open(Output.strip('.msh')+'.geo','w')
+    g.write('// Gmsh proyect\n')
+#-----------------Physical entities 1D-----------------------------------
+    if Nodes.ndim==1:
+        xmin=str(Nodes[0])
+        xmax=str(Nodes[Nodes.size-1])
+       
+        g.write('Point(1)={'+xmin+',0,0,1.0};\n')
+        g.write('Point(2)={'+xmax+',0,0,1.0};\n')
+        g.write('Line(3)={1,2};\n')
+    else:
+        print 'dimension not supperoted yet'
+    g.close()
+#------------------------------------------------------------------------
+#------------------------ Headers for .msh file------------------------
     f.write('$MeshFormat\n')
     f.write('2.2 0 8\n')
     f.write('$EndMeshFormat\n')
-    f.write('$Nodes\n')
-    vec=np.zeros((np.shape(points)[0],1))
-    for i in range(0,np.shape(points)[0]):
-        vec[i,0]=i+1
+#------------------------ Nodes for the .msh file------------------------            
     
-    points=hstack((vec,points))
+    f.write('$Nodes\n')
+    vec=np.zeros((np.shape(Nodes)[0],1))
+    for i in range(0,np.shape(Nodes)[0]):
+        vec[i,0]=i+1      # Enumeration of nodes
+    if Nodes.ndim==1:
+        Nodes=np.array([Nodes]).T
+        
+        # Conversion for coordinates with y and z
+        Nodes=hstack((Nodes,np.zeros((np.shape(Nodes)[0],2)))) 
+    nPoi=str(np.shape(Nodes)[0])
+    Nodes=hstack((vec,Nodes)) # Enumeration added to the array
     f.write(nPoi+'\n')
-    np.savetxt(f,points,fmt='%d %f %f %f')
+    np.savetxt(f,Nodes,fmt='%d %f %f %f')
     f.write('$EndNodes\n')
+#------------------------ Elements for the .msh file------------------------  
+
     f.write('$Elements\n')
     f.write(nEle+'\n')
-    vec=np.zeros((np.shape(triangles)[0],5))
-    for i in range(0,np.shape(triangles)[0]):
-        vec[i,0]=i+1
-        vec[i,1:5]=[2,2,0,6]
-    triangles =triangles[:,1:5]
-    triangles=hstack((vec,triangles))
-    print triangles.shape
-    np.savetxt(f,triangles,fmt='%d %d %d %d %d %d %d %d')
+    nEle=int(nEle)
+    vec=np.zeros((nEle,5))
+    NodPerEl=Elems.ndim   # For line elements there should be 2 nodes per element
+    # Point elements -------------------------------------------------------
+    if NodPerEl==2:
+        ePoints=np.zeros((2,6))
+        ePoints[0,:]=[1,15,2,0,1,1]
+        ePoints[1,:]=[2,15,2,0,2,nNodes]
+        np.savetxt(f,ePoints,fmt='%d')
+        
+    #-----------------------------------------------------------------------
+
+    
+    # Line elements 1D -----------------------------------------------------
+    
+    
+    if NodPerEl==2:
+        for i in range(0,nEle):
+            vec[i,0]=i+3
+            
+            vec[i,1:5]=[1,2,0,3]
+##        Elems =Elems[:,1:5]
+    #-----------------------------------------------------------------------
+
+    # Line elements 2D -----------------------------------------------------        
+    else:
+        for i in range(0,nEle):
+            vec[i,0]=i+1
+            vec[i,1:5]=[2,2,0,6]
+        Elems =Elems[:,1:5]
+    #-----------------------------------------------------------------------
+        
+    Elems=hstack((vec,Elems))
+##    print Elems
+    
+   
+    if NodPerEl==2:
+        
+        np.savetxt(f,Elems,fmt='%d %d %d %d %d %d %d')
+    else:
+        np.savetxt(f,Elems,fmt='%d %d %d %d %d %d %d %d')
     f.write('$EndElements\n')
-def WriteSolverInput(Output,Dimension=1,BCType='Dir',params=[],Eq='Schro',\
+def WriteSolverInput(Output,Dimension=1,BCType='Dir',parameter=[],Eq='Schro',\
                      Type='Stationary'):
 
     f=open(Output,'r+')
@@ -90,8 +168,8 @@ def WriteSolverInput(Output,Dimension=1,BCType='Dir',params=[],Eq='Schro',\
     Dimension =str(Dimension)
     f.write(Dimension +'\n')
     f.write(BCType +'\n')
-    if params !=[]:
-        np.savetxt(f,params)
+    if parameter !=[]:
+        np.savetxt(f,parameter,fmt='%f')
     f.write(Eq +'\n')
     f.write(Type +'\n')
     f.write('$End Solver input\n')
