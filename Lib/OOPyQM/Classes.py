3273ee6ead8500376ee30eeafb685dfde4d6e17f
Santiago <santiag77e@gmail.com>
2013-02-10 21:44:57 -0500

    Huge block of files that I should have commited before but didn't.  These are the temprary libraries that will hold the Object Oriented version of PeYeQM.
    Classes.py contains definition of the classes that are needed to set up a simulation. This is:
     + Simulation
            + Domain
                  + Elements
                      +triangles
                  + Nodes
     And such. Interpreter contains clases that are used to build the corresponding set of linear equations to the simulation defined before.
    
    I will start commiting more often now that the OOP version has been uploaded.

---
+++ Lib/OOPyQM/Classes.py
@@ -0,0 +1,351 @@
+# -*- coding: utf-8 -*-
+"""
+Module Classes will temporalry hold the classes used by PeYeQM to
+define instances.
+"""
+__author__ = ['Santiago Echeverri Chacón']
+
+class Simulation():
+    """
+    Instances from simulation class will act as a container for the overal 
+    problem definition of the simulation.
+    It shall include information about:
+        * The Domain and its discretization.
+        * Options and configuration.
+        *Type of simulations and solvers to be used.
+    """
+    def __init__(self):
+        self.domain = Domain()
+    sim_type = 'QM'
+    time_dependency = 'Stationary'
+    dimension = 2
+
+    def read_solver_input(self, filename):
+        """
+        Reads the solver input from a file of gmsh ASCII format V2.2.
+        This input is to be read by the Solver module.
+        Parameters:
+        -----------
+        filename: String which contain filename and path of the output file.
+        Returns:
+        --------
+        solver_input: List with the following parameters:
+    
+        [dimension,bc_type,parameter,eq,sol_type,analysis_param]
+    
+        dimension:  int parameter that tells the program wether to solve for a
+                1D problem or a 2D problem (not supported yet)
+    
+        bc_type:     String parameter for the selection of a border condition
+                    that can be either:
+    
+                        'Dir'   For the Dirichlet border condition
+                                (Infinite potential well).
+    
+                        'Bloch' For the periodic formulation of the problem.
+                                (Electron in a periodic material )
+                
+        parameter:  Is an array that describes the potential actuating over the
+                    the elements of the domain given by Elems. For each element in
+                    Elems there is an associated potential value on the same
+                    position in the array parameter.
+    
+                    The potential in Scroedinger equation defines the specific
+                    nature of the problem to be solved. For more details on how
+                    to define a potential and what does it mean pleas read the
+                    documentation of the Potential1D function in the module PrePro.
+    
+       
+        eq:         Can be 'Schroedinger' or another not implemented yet
+        
+    
+        sol_type:       String that tells wether to solve the stationary version of
+                    the equation or another not yet suported.
+    
+                    'Stationary'   
+    
+        analysis_param:   Array that contains the information regarding the number
+                         of solutions to be computed and wether to save the values
+                         or not.
+    
+                        analysis_param[0]:  String  answer to the question
+                                                   save  Eigen Values?
+                        analysis_param[1]:  String  answer to the question
+                                                   save  Eigen Vectors?
+                        analysis_param[2]:  Integer  number of Eigen Values to save
+                        analysis_param[3]:  Integer  number of Eigen Vectors to save 
+                        analysis_param[4]:  Integer number of wave numbers 
+                                            in x to sweep
+                        analysis_param[5]:  Integer number of wave numbers 
+                                            in y to sweep
+                        analysis_param[6]:  biggest value of k. it may be the lenght
+                                            of the dommain                                        
+                        
+        bc_filename:    string that tells where to look for the boundary 
+                        conditions  
+                            
+         Last modification: date 14/11/2011
+        """
+        from read_mesh import read_solver_input
+        solver_input = read_solver_input(filename) 
+        if solver_input != []:
+            self.dimension = solver_input[0]
+            self.bc_type = solver_input[1]
+            self.body_parameter = solver_input[2]                # Reasign
+            self.sim_type = solver_input[3]
+            self.time_dependency = solver_input[4]
+            self.bloch_param = solver_input[5]         # the solution 
+            self.bc_filename = solver_input[6]
+        else:
+            print 'Pending implementation'
+    
+
+class Domain():
+    """ 
+    Instances from Domain Class contain the attributes that define 
+    characteristics of the simulation such as regions, meshing, and 
+    boundary conditions.
+    To be included here are instances of other clases such as:
+        * Nodes.
+        * Elements 
+        * And boundaries.
+    """
+    def __init_(self):
+        self.nodes = 0
+        self.elements = 0
+    
+    def read_mesh_file(self, filename):
+        from read_mesh import read_mesh
+        
+        self.nodes = Nodes()
+        self.elements = Elements()
+  
+        _nodes, _elements = read_mesh(filename)        
+        
+        self.nodes.add(_nodes)
+        self.elements.add(_elements)
+     
+    
+    def read_bc_file(self, filename):
+        from read_mesh import read_bc
+        self.boundaries = Boundaries()
+        _bc = read_bc(filename)
+        self.boundaries.add(_bc)                
+                  
+class Nodes():
+    """
+    Nodes, will be a instance fo handling nodes. Right now its only 
+    two attributes are the total number of nodes and the array of 
+    ccordinates.
+    """
+    # def __init__(self):
+        
+    def add(self, _nodes):        
+        from numpy import shape
+        self.n = shape(_nodes)[0]                 #Number of nodes
+        self.coords = _nodes        
+    
+    
+class Elements():
+    """
+    Elements for now counts the number f line and triangle elements
+    in a format imported from a file by using function read_mesh().
+    
+    The Idea is that the instance elements contains all the attributes 
+    associated with elements
+    """
+    #Lines = 0    
+  #  def __init__(self, _elements):
+    def add(self, _elements):
+        from numpy import shape
+        self.lines = _elements['Lines']
+        self.n_lines = shape(self.lines)[0]
+        if 'Triangles' in _elements:
+            self.triangles = Triangles(_elements['Triangles'])
+            
+            
+    #It doesn't look so hard to implement square elements
+
+    # It would be cool to have a function and or attributes where 
+    # The order of elements is taken into account. 
+    #Things such as an element properties like:
+    #   * Order
+    #   * Area, or other shape attributes like jacobian
+    #   * Local matrices...
+    
+    
+class Triangles():
+    """
+    Triangles are one kind of very important elements
+    """    
+    def __init__(self, raw_triangles):
+        from numpy import shape
+        self.el_set = raw_triangles
+        self.n_elements = shape(self.el_set)[0]
+        #== Assign a local stiffness for the elements given the order===
+        if shape(self.el_set)[1] == 4:
+            self.order = 1
+
+    def extract_el_points(self, _nodes, el_id):
+        """
+        Extracts the coordinates of the nodes in each triangular element and
+        retrieves each vertice as a vector of two dimensions.
+        
+        Parameters:
+        -----------
+        nodes:  numpy array of dimension (n_nodes, 3), where n_nodes is the 
+                number of nodes forming the mesh, and the three columns 
+                represent coordinates (x, y, z).
+        
+        triangles:  numpy array of dimension (n_triangles, 4), where n_nodes 
+                    is the number of nodes forming the mesh, the first is the 
+                    label that refers to the physical entity where each node 
+                    belongs. The remaining columns  tell which nodes belong
+                    to each of the vertices of the triangle.
+                    
+        el:   integer value of the current element
+        
+        Returns:
+        --------
+        pt_a, pt_b, pt_c:  vector like array of two dimensions where the first 
+                           is the position in x, and the second is the position 
+                           of the node in y.  
+        """
+        from numpy import zeros
+        if 'el_set' in self.__dict__:
+            if self.order == 1:
+                p1_triangles = self.el_set        
+            else:
+                print 'No higher order elements yet, there must be an error'
+        else:
+            print 'No elements parsed, do something else'
+            
+        pt_a = zeros(2)
+        pt_b = zeros(2)
+        pt_c = zeros(2)
+    
+        pt_a[0] = _nodes[p1_triangles[el_id, 1] - 1, 0]
+        pt_a[1] = _nodes[p1_triangles[el_id, 1] - 1, 1]
+        pt_b[0] = _nodes[p1_triangles[el_id, 2] - 1, 0]
+        pt_b[1] = _nodes[p1_triangles[el_id, 2] - 1, 1]
+        pt_c[0] = _nodes[p1_triangles[el_id, 3] - 1, 0]
+        pt_c[1] = _nodes[p1_triangles[el_id, 3] - 1, 1]
+        return [pt_a, pt_b, pt_c]
+        
+    def extract_el_edges(self, points):
+        """
+        Receive the list of points given by "extract_el_points" and return 
+        a list of line segments from which the area can be calculated
+        """
+        pt_a, pt_b, pt_c = points 
+        #print pt_a, pt_b, pt_c
+        ln_ab = pt_b - pt_a    
+        ln_bc = pt_c - pt_b
+        ln_ca = pt_a - pt_c    
+        lines = [ln_bc, ln_ca, ln_ab]        
+        return lines
+        
+    def calc_area(self, lines):
+        """
+        Calculates the area of a triangle by using Heron's theorem
+        
+        Parameters:
+        -----------
+        line:  list of 3 2D arrays. Defines line segments of the triangle
+
+        Returns:
+        --------
+        area:  float number.          
+        """
+        from math import sqrt
+        from numpy.linalg import norm
+    
+        mg_ab = norm(lines[0]) # Calculate the magnitude (mg) of each line
+        mg_bc = norm(lines[1])
+        mg_ca = norm(lines[2])
+            
+        s = (mg_ab + mg_bc + mg_ca)/2    # Define the semiperimeter (s)
+                                         # for each triangle
+       
+        area = sqrt(s*(s-mg_ab)*(s-mg_bc)*(s-mg_ca))
+        return area
+    
+        
+    def build_local_stiffness(self, nodes, el_id):
+        if self.order == 1:
+            from numpy import zeros, dot
+            points = self.extract_el_points(nodes, el_id)
+            lines = self.extract_el_edges(points)
+            calc_area = self.calc_area
+            area = calc_area(lines)
+            n = len(lines)
+            lo_stiff =  zeros((n, n))  # local (lo) stiffness (stiff) matrix
+            for i in range(n):
+                for j in range(n):
+                    lo_stiff[i, j] = dot(lines[i], lines[j]) / (4*area) 
+            return lo_stiff
+        else:
+            print 'No higher order triangles for now'
+        
+    def local_mass_matrix(self, nodes, el_id):
+        if self.order == 1:
+            from numpy import array, zeros
+            from numpy.linalg import det
+            tr_mat = zeros((2,2))
+            lo_mass = 1./24*array([[2, 1, 1],\
+                                   [1, 2, 1],\
+                                   [1, 1, 2]])
+            points = self.extract_el_points(nodes, el_id)
+            tr_mat[:, 0] = points[1] - points[0]
+            tr_mat[:, 1] = points[2] - points[0]
+            
+            jac = det(tr_mat)  
+            
+            lo_mass = jac * lo_mass    # Escalate acording with the jacobian of the
+                                       # transformation 
+        else:
+            print 'No higher order triangles for now'
+        return lo_mass
+        
+    #======================== Local Newman matrix  1D P1 ===========================
+    def local_newman_matrix():
+        from numpy import array
+        lo_new = 1./6* array([[2, 1],\
+                              [1, 2]])
+        return lo_new
+    #======================== Local potential matrix  2D P1 =======================
+    def local_potential_matrix(self, nodes, el_set, v, el):
+        if self.order == 1:
+            from numpy import array, zeros
+            from numpy.linalg import det
+            vec_v = zeros((3,1))   
+            vec_v[0, 0] = v[el_set[el, 1]-1]
+            vec_v[1, 0] = v[el_set[el, 2]-1]
+            vec_v[2, 0] = v[el_set[el, 3]-1]
+            tr_mat = zeros((2,2))      # Initiate the transformation (tr) matrix (mat)
+            pt_a, pt_b, pt_c = self.extract_el_points(nodes, el)
+            tr_mat[:, 0] = pt_b - pt_a
+            tr_mat[:, 1] = pt_c - pt_a
+            jac = det(tr_mat)  
+            lo_v = 1./60*array([[3, 1, 1],\
+                                  [1, 3, 1],\
+                                  [1, 1, 3]]) # Local (lo) potential (v) matrix
+           
+            lo_v = jac * (lo_v * vec_v)  # Escalate acording with the jacobian 
+                                       # of the transformation 
+        
+        else:
+            print 'No higher order triangles for now'
+        return lo_v
+class Boundaries():
+    """
+    Define what tags from physical entities are associated with certain 
+    boundary conditions.
+    """
+    def add(self, _bc):
+        self.dirichlet = _bc['Dir']
+        self.newman = _bc['New']        
+        if 'Bloch' in _bc:
+            self.bloch = _bc['Bloch']
+            
+                        
---
+++ Lib/OOPyQM/Interpreter.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+"""
+Module interpreter. Will be a place to temorarily hold the class that
+receives a simulation file and then assembles the linear equations to solve
+"""
+__author__ = ['Santiago Echeverri Chacón']
+
+class Interpreter():
+    """
+    Instances from this class are meant to receive a simulation object and
+    construct from it the linear equations that then get to be solved by 
+    an appropiate solver.
+    """
+    def build_QM_dirichlet_eq(self,simulation):
+
+        h = 1.
+        m = 1.        
+        print 'Building matrices...\n'        
+        stif = self.global_stiffness_matrix(simulation)
+        stif = h/(2.*m)*stif 
+        [stif_d, d, remove, g] = self.dirichlet_vector(simulation, stif)
+        mass_d = self.global_mass_matrix(simulation, remove)
+        v_d = self.global_potential_matrix(simulation, remove)
+        equation = {'left_side': stif_d + v_d,'right_side':mass_d}
+        return equation
+        
+    def build_QM_bloch_eq(self, simulation):
+        from numpy import asarray,vstack
+        h = 1.
+        m = 1.        
+        print 'Building matrices...\n'        
+        stif = self.global_stiffness_matrix(simulation)
+        stif = h/(2.*m)*stif 
+        mass = self.global_mass_matrix(simulation)
+        v = self.global_potential_matrix(simulation)
+        #================ Convert to complex matrices ====================
+        stif = asarray(stif, dtype = complex)
+        mass = asarray(mass, dtype = complex) 
+        v = asarray(v, dtype = complex)
+         #===== Sum kinetic and potential matrices of the Hamiltonian ======
+        stif = stif + v
+        bloch = simu.domain.boundaries.bloch      
+        #Will continue...
+        
+    def global_stiffness_matrix(self, simulation):
+        """
+        Funtion for the assembly of the global stiffnes matrix of a 
+        simulation file. 
+        
+        Parameters:
+        -----------
+        simulation: Instance of class Simulation( ) which contains all 
+                    of the information needed for an analysis.
+                    Read more about the structure of this class somewhere.
+        
+        Returns:
+        --------
+        
+        glo_stif:  Matrix like array of size (n_nodes, n_nodes). Represents
+                   the global stiffness matrix of the system
+        """
+        from numpy import zeros
+        # Discuss this error with Edward      
+        #print isinstance(simulation, Simulation)
+        #from Classes import Simulation
+        #print isinstance(simulation, Simulation)
+        if True:
+            nodes_coords = simulation.domain.nodes.coords            
+            n_nodes = simulation.domain.nodes.n
+            
+            sim_elements = simulation.domain.elements
+            if simulation.dimension == 1:
+                elements = sim_elements.lines
+                # Pending to add 1D support
+            elif simulation.dimension == 2:
+                if 'triangles' in sim_elements.__dict__:
+                    elements = sim_elements.triangles
+                else:
+                    print 'Wait untill other elements are supported'
+            else:
+                print 'No 3 dimensional simulations supperted'
+            
+            n_elements = elements.n_elements
+            el_set = elements.el_set
+        
+            glo_stif = zeros((n_nodes, n_nodes))            
+            
+            for el in range(n_elements):
+                lo_stif = elements.build_local_stiffness(nodes_coords, el)
+                for i in range(1, lo_stif.shape[0]+1):
+                    for j in range(1, lo_stif.shape[0]+1):
+                        
+                        glo_stif[el_set[el, i]-1, el_set[el, j]-1] = \
+                        glo_stif[el_set[el, i]-1, el_set[el, j]-1] + \
+                        lo_stif[i-1,j-1]
+                        #                      ^ due to python's 0 base numbering
+            return glo_stif
+        else:
+            print 'Wrong class. input argument should be an instance of simulation.'
+            
+    def dirichlet_vector(self, simulation, glo_stif):
+        """
+        This function computes the d vector associated to Dirichlet
+        boundary conditions on certain nodes of the domain. 
+        
+        Parameters:
+        -----------
+        bc_lines:  numpy array of dimension (n_lines, 3), where the first column
+                   is the label that refers to the physical entity where each 
+                   node belongs, and the other two columns represent node 1 and
+                   2 of the line element
+                
+        dirichlet: list of lists. the first index refers to lists that describe
+                   one particular Dirichlet condition, where the first elements 
+                   of this list is the tag of the physical entity, and the 
+                   second is an array that has the values over each degree of 
+                   freedom.
+                   
+        glo_stiff: matrix like array of size (n_nodes,n_nodes) called
+                   the stiffness matrix of the system.
+                   
+           
+        Returns:
+        --------
+        glo_stif_d:  global stiffness matrix reduced by removing the rows and 
+                     columns asociated with the dirichlet conditions.
+        d:        Vector associated with siffness matrix over dirichlet nodes.
+        remove:   List with the numbers of columns to be removed.
+            
+        """ 
+        from numpy import zeros, dot, delete 
+        
+        #assert isinstance(simulation, Simulation)        
+        assert 'lines' in simulation.domain.elements.__dict__
+        bc_lines = simulation.domain.elements.lines
+        dirichlet = simulation.domain.boundaries.dirichlet
+        n_nodes = glo_stif.shape[0]         # Number (n) of nodes
+        n_lines = simulation.domain.elements.n_lines         # Number of lines
+        
+#not needed        n_bc_d = len(dirichlet)  # Number of Dirichlet boundary conditions
+        remove = []              # This vector will tell which rows and columns
+                                 # should be removed 
+        g = zeros(n_nodes)
+        for tag in dirichlet:
+            value = dirichlet[tag][0][0]
+            
+            for ln in range(n_lines):
+                
+                if bc_lines[ln, 0] == int(tag):
+                    g[bc_lines[ln, 1]-1] = value
+                    g[bc_lines[ln, 2]-1] = value
+                    
+                    if bc_lines[ln, 1]-1 not in remove: # makes a list for removing
+                        remove.append(bc_lines[ln, 1]-1)# lines and columns.
+                    if bc_lines[ln, 2]-1 not in remove:
+                        remove.append(bc_lines[ln, 2]-1)
+            
+        d = dot(glo_stif, g)
+        d = delete(d, remove)
+        
+        glo_stif = delete(glo_stif, remove, 0)
+        glo_stif = delete(glo_stif, remove, 1)
+        
+        return glo_stif, d, remove, g
+        
+    def global_mass_matrix(self, simulation, *remove):
+        """
+        funtion for the assembly of the global mass matrix.
+        
+        Parameters:
+        -----------
+        simulation: Instance of class Simulation( ) which contains all 
+                    of the information needed for an analysis.
+                    Read more about the structure of this class somewhere.
+                    
+        remove:     List with the numbers of columns to be removed
+        Returns:
+        --------
+        glo_mass: Global mass matrix where the dirichlet positions have 
+                  been removed.
+        """
+        from numpy import zeros, delete
+        #If simulation is an instance of Classes.Simulation()
+        if True:
+            nodes_coords = simulation.domain.nodes.coords            
+            n_nodes = simulation.domain.nodes.n
+            sim_elements = simulation.domain.elements
+            if simulation.dimension == 1:
+                elements = sim_elements.lines
+                # Pending to add 1D support
+            elif simulation.dimension == 2:
+                if 'triangles' in sim_elements.__dict__:
+                    elements = sim_elements.triangles
+                else:
+                    print 'Wait untill other elements are supported'
+            else:
+                print 'No 3 dimensional simulations supperted'
+            n_elements = elements.n_elements
+            el_set = elements.el_set
+            glo_mass = zeros((n_nodes, n_nodes))# Initiate Global (glo) mass matrix
+            
+            for el in range(n_elements):
+                #pending for iteration over elements }
+                # call to local_mass_matrix
+                lo_mass = elements.local_mass_matrix(nodes_coords, el)
+                for i in range(1, lo_mass.shape[0]+1):
+                    for j in range(1, lo_mass.shape[0]+1):                       
+                        glo_mass[el_set[el, i]-1, el_set[el, j]-1] = \
+                        glo_mass[el_set[el, i]-1, el_set[el, j]-1] + \
+                        lo_mass[i-1,j-1]
+            glo_mass_d = delete(glo_mass, remove, 0)
+            glo_mass_d = delete(glo_mass_d, remove, 1)
+            return glo_mass_d
+             
+        else:
+            print 'Wrong class. input argument should be an instance of simulation.'
+        
+        
+    def global_potential_matrix(self, simulation, *remove):
+        """
+            funtion for the ensamble of the global potential matrix
+            
+            Parameters:
+            -----------
+            simulation: Instance of class Simulation( ) which contains all 
+                    of the information needed for an analysis.
+                    Read more about the structure of this class somewhere.
+                    
+            remove:     List with the numbers of columns to be removed
+                        
+            Returns:
+            --------
+        """
+        from numpy import zeros, delete
+        #If simulation is an instance of Classes.Simulation()
+        if True:
+            nodes_coords = simulation.domain.nodes.coords            
+            n_nodes = simulation.domain.nodes.n
+            sim_elements = simulation.domain.elements
+            v = simulation.body_parameter
+            if simulation.dimension == 1:
+                elements = sim_elements.lines
+                # Pending to add 1D support
+            elif simulation.dimension == 2:
+                if 'triangles' in sim_elements.__dict__:
+                    elements = sim_elements.triangles
+                else:
+                    print 'Wait untill other elements are supported'
+            else:
+                print 'No 3 dimensional simulations supperted'
+            n_elements = elements.n_elements
+            el_set = elements.el_set
+            glo_v = zeros((n_nodes, n_nodes))# Initiate Global (glo) mass matrix
+            
+            for el in range(n_elements):
+                #pending for iteration over elements }
+                # call to local_mass_matrix
+                lo_v = elements.local_potential_matrix(nodes_coords, el_set, v, el)
+                for i in range(1, lo_v.shape[0]+1):
+                    for j in range(1, lo_v.shape[0]+1):                       
+                        glo_v[el_set[el, i]-1, el_set[el, j]-1] = \
+                        glo_v[el_set[el, i]-1, el_set[el, j]-1] + \
+                        lo_v[i-1,j-1]
+            glo_v_d = delete(glo_v, remove, 0)
+            glo_v_d = delete(glo_v_d, remove, 1)
+            return glo_v_d
+             
+        else:
+            print 'Wrong class. input argument should be an instance of simulation.'
+        
