592eaec269280e740194c506fb9569bd3d9a027f
Santiago <santiag77e@gmail.com>
2013-02-14 16:11:02 -0500

    Fixed some typos that fucked up the basis functions. When the Jacobian was evaluated on the coordinates of the natural element it was different to the identity matrix.
    Also added a code snipett that checks if the basis functions work correctly.

--- Lib/OOPyQM/Sympy tests.py
+++ Lib/OOPyQM/Sympy tests.py
@@ -19,7 +19,7 @@ for i in range(1,9):
     y[i] = Symbol('y'+str(i))
 
 h[5] = Rational(1,2)*(1-r**2)*(1+s)
-h[6] = Rational(1,2)*(1-s**2)*(1+r)
+h[6] = Rational(1,2)*(1-s**2)*(1-r)
 h[7] = Rational(1,2)*(1-r**2)*(1-s)                                
 h[8] = Rational(1,2)*(1-s**2)*(1+r)
 h[1] = Rational(1,4)*(1+r)*(1+s)-Rational(1,2)*h[5] \
@@ -28,7 +28,7 @@ h[2] = Rational(1,4)*(1-r)*(1+s)-Rational(1,2)*h[5] \
                                 -Rational(1,2)*h[6]
 h[3] = Rational(1,4)*(1-r)*(1-s)-Rational(1,2)*h[6] \
                                 -Rational(1,2)*h[7]
-h[4] = Rational(1,4)*(1+r)*(1+s)-Rational(1,2)*h[7] \
+h[4] = Rational(1,4)*(1+r)*(1-s)-Rational(1,2)*h[7] \
                                 -Rational(1,2)*h[8]
 X = 0
 for i in range(1, 9):
@@ -36,20 +36,23 @@ for i in range(1, 9):
 Y = 0
 for i in range(1, 9):
     Y = Y + h[i]*y[i]   
-#pprint(X)
+pprint(X)
 J = zeros((2,2))
 J[0,0] = diff(X,r)
 J[0,1] = diff(X,s)
 J[1,0] = diff(Y,r)
 J[1,1] = diff(Y,s)
 
-pprint(J)
-    
-                              
-#J = zeros((8,8))
-#for i in range(8):
-#    for j in range(8):
-#        J[i,j] = diff(X,
-                                
-                             
-pprint((h[3]).subs({r:-1,s:-1}))
+jac_det = J.det()
+
+# Test to see if weight functions work
+nodes=[{r:1,s:1},{r:-1,s:1},{r:-1,s:-1},{r:1,s:-1},{r:0,s:1},{r:-1,s:0},\
+          {r:0,s:-1},{r:1,s:0}]
+for j in range(1,9):
+    print 'function h_',j
+    for i in range(8):
+        pprint(h[j].subs(nodes[i]))     
+#--------------------------------------
+pprint(J.subs({x[1]:1, y[1]:1, x[2]:-1, y[2]:1, x[3]:-1, y[3]:-1, \
+                 x[4]:1,y[4]:-1, x[5]:0,y[5]:1, x[6]:-1,y[6]:0,\
+                 x[7]:0,y[7]:-1, x[8]:1,y[8]:0}))
